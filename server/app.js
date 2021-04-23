b774a094755f14061df91b64bf6ae42a4cdfb80c
Dale <djs93@njit.edu>
2021-04-22 21:03:54 -0400

    Add landing page & update footer

--- server/app.js
+++ server/app.js
@@ -1,17 +1,25 @@
 'use strict';
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
 const open = require("open");
 const bodyParser = require('body-parser');
-var cors = require('cors')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 app.use(cors())
-app.use(express.static('docs'));
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+
+// To parse cookies from the HTTP Request
+app.use(cookieParser());
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
 
 // Require employee routes
 const citiesRoutes = require('./routes/cities.routes');
@@ -21,6 +29,14 @@ app.use('/api/v1/cities', citiesRoutes);
 
 app.set('port', process.env.PORT || 8000);
 app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+app.set('view engine', 'hbs');
+app.set('views', 'docs/views');
+
+app.get('/', function (req,res){
+    res.render('home');
+});
+
+app.use(express.static('docs'));
 
 app.listen(app.get('port'), function() {
     console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
