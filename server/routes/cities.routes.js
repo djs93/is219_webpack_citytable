0b3ec128e6cbdcc440b548f31af82c8aa0220579
Dale <djs93@njit.edu>
2021-04-25 15:02:42 -0400

    Add authentication to cities api requests

--- server/routes/cities.routes.js
+++ server/routes/cities.routes.js
@@ -1,20 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const cityController = require('../controllers/cities.controllers');
+const jwt = require('jsonwebtoken');
+
+const accessTokenSecret = 'can the semester end already';
+
+authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 // Retrieve all employees
-router.get('/', cityController.findAll);
+router.get('/', authenticateJWT, cityController.findAll);
 
 // Create a new employee
-router.post('/', cityController.create);
+router.post('/', authenticateJWT, cityController.create);
 
 // Retrieve a single employee with id
-router.get('/:id', cityController.findById);
+router.get('/:id', authenticateJWT, cityController.findById);
 
 // Update a employee with id
-router.put('/:id', cityController.update);
+router.put('/:id', authenticateJWT, cityController.update);
 
 // Delete a employee with id
-router.delete('/:id', cityController.delete);
+router.delete('/:id', authenticateJWT, cityController.delete);
 
 module.exports = router
