39dec9a519849924e9f1fb488bebd534cc207bd6
Dale <djs93@njit.edu>
2021-04-25 19:18:07 -0400

    Set up Passport.js with Node and Express

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 'use strict';
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+require("dotenv").config();
 const app = express();
 const open = require("open");
 const bodyParser = require('body-parser');
@@ -16,11 +22,63 @@ app.use(bodyParser.json());
 // Require employee routes
 const citiesRoutes = require('./routes/cities.routes');
 
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
 // using as middleware
 app.use('/api/v1/cities', citiesRoutes);
 
 app.set('port', process.env.PORT || 8000);
 app.set('ip', process.env.NODEJS_IP || '127.0.0.1');
+app.set('view engine', 'hbs');
+app.set('views', 'docs/views');
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+app.use(expressSession(session));
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 app.listen(app.get('port'), function() {
     console.log('%s: Node server started on %s ...', Date(Date.now()), app.get('port'));
