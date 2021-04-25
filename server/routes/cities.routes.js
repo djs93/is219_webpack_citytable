0b22065387982012a1e67640c6d9b021af08fac6
Dale <djs93@njit.edu>
2021-04-25 15:24:13 -0400

    Add admin authentication to data-changing cities api requests

--- server/routes/cities.routes.js
+++ server/routes/cities.routes.js
@@ -24,19 +24,43 @@ authenticateJWT = (req, res, next) => {
     }
 };
 
-// Retrieve all employees
+// Retrieve all city
 router.get('/', authenticateJWT, cityController.findAll);
 
-// Create a new employee
-router.post('/', authenticateJWT, cityController.create);
+// Create a new city
+router.post('/', authenticateJWT, (req, res) => {
+    const { role } = req.user;
 
-// Retrieve a single employee with id
+    if (role !== 'admin') {
+        return res.sendStatus(403);
+    }
+
+    cityController.create(req, res);
+});
+
+// Retrieve a single city with id
 router.get('/:id', authenticateJWT, cityController.findById);
 
-// Update a employee with id
-router.put('/:id', authenticateJWT, cityController.update);
+// Update a city with id
+router.put('/:id', authenticateJWT, (req, res) => {
+    const { role } = req.user;
 
-// Delete a employee with id
-router.delete('/:id', authenticateJWT, cityController.delete);
+    if (role !== 'admin') {
+        return res.sendStatus(403);
+    }
+
+    cityController.update(req, res);
+});
+
+// Delete a city with id
+router.delete('/:id', authenticateJWT, (req, res) => {
+    const { role } = req.user;
+
+    if (role !== 'admin') {
+        return res.sendStatus(403);
+    }
+
+    cityController.delete(req, res);
+});
 
 module.exports = router
